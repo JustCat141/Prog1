647efbe7a2d6e334f39085ac3b65de7935f8f820
JustCat141 <patreszkaa@gmail.com>
2022-04-09 00:45:16 +0200

    update on drill20

--- drill20/main.cpp
+++ drill20/main.cpp
@@ -1,29 +1,72 @@
 #include "std_lib_facilities.h"
 
-template<typename Iterator>
-Iterator high(Iterator first, Iterator last)
-// return an iterator to the element in [first:last) that has the highest value
+// ???
+template<typename Iter1, typename Iter2>
+Iter2 my_copy(Iter1 f1, Iter1 e1, Iter2 f2)
 {
-    Iterator high = first;
-    for (Iterator p = first; p!=last; ++p)
-        if (*high<*p) high = p;
-    return high;
+    for (Iter1 p = f1; p != e1; ++p)
+        *f2++ = *p;
+    return f2;
+}
+
+template<typename T>
+void print_container(T container)
+{
+    for(auto& elem: container)
+        cout << elem << ", ";
+
+    cout << endl;
+}
+
+template<typename T>
+void AddVals(T& type, int value)
+{
+    for(auto& elem : type)
+        elem += value;
 }
 
 int main()
 {
     try
     {
-        int array_int[10] = {0,1,2,3,4,5,6,7,8,9};
-        vector<int> vector_int;
-        list<int> list_int;
+        int values[10] = {0,1,2,3,4,5,6,7,8,9};
 
-        for (int i = 0; i < 10; i++)
-        {
-            vector_int.push_back(i);
-            list_int.push_back(i);
-        }
-        
+        array<int, 10> array_int;
+        copy(values,values + 10, array_int.begin());
+        print_container(array_int);
+
+        vector<int> vector_int(10);
+        copy(values,values+10,vector_int.begin());
+        print_container(vector_int);
+
+        list<int> list_int(10);
+        copy(values,values+10,list_int.begin());
+        print_container(list_int);
+
+        array<int, 10> array_copy = array_int;
+        vector<int> vector_copy = vector_int;
+        list<int> list_copy = list_int;
+
+        AddVals(array_copy, 2);
+        AddVals(vector_copy, 3);
+        AddVals(list_copy, 5);
+
+        print_container(array_copy);
+        print_container(vector_copy);
+        print_container(list_copy);
+
+        array<int,10> valami;
+        my_copy(array_copy.begin(),array_copy.end(),valami.begin());
+        print_container(valami);
+
+        array<int,10>::iterator search;
+        search = find(valami.begin(), valami.end(), 3);
+
+        if(search != valami.end())
+            cout << "Value found at index: " << distance(valami.begin(),search) << endl;
+        else
+            cout << "Value not found!" << endl;
+
         return 0;
     }
     catch(const std::exception& e)
